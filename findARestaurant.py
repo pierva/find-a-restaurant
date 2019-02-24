114d745a2ee54745c4c2353bfd16e03b433944b7
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-24 17:54:16 -0500

    feat: Add method to get coordinates for a given address with geocoderAPI

---
+++ findARestaurant.py
@@ -0,0 +1,18 @@
+import json
+import requests
+import config
+
+def getGeocodeLocation(address):
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+    params = {
+                'sensor': 'false',
+                'address': address,
+                'key' : config.GEOCODER_API_KEY
+             }
+    r = requests.get(url, params=params)
+    results = r.json()
+    if results['status'] == 'OK':
+        location = results['results'][0]['geometry']['location']
+        return location
+    else:
+        return results['status']
