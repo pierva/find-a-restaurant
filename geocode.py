e5ac163808b158dfa99a22ad9866584f16a3a422
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-24 18:02:20 -0500

    feat: Add feature to get venue picture

--- geocode.py
+++ geocode.py
@@ -19,3 +19,22 @@ def getGeocodeLocation(address):
             return 'No restaurants available.'
     else:
         return "Error code %s.\n %s" % (results['meta']['code'], results['meta']['errorDetail'])
+
+def getVenuePicture(venueId):
+    url = 'https://api.foursquare.com/v2/venues/%s/photos?' % venueId
+    params = {
+                'client_id': config.foursquare_client_id,
+                'client_secret': config.foursquare_secret,
+                'v': config.foursquare_v,
+                'limit': 1
+             }
+    r = requests.get(url, params=params)
+    results = r.json()
+    if results['meta']['code'] == 200:
+        details = results['response']['photos']['items']
+        if len(details) > 0:
+            return details[0]['prefix'] + "300x300" + details[0]['suffix']
+        else:
+            return 'No picture available.'
+    else:
+        return "Error code %s.\n %s" % (results['meta']['code'], results['meta']['errorDetail'])
