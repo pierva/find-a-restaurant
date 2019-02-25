bb903a91695ecef3805900467cbc8d127d5b188a
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-25 14:13:15 -0500

    fix: Update findRestaurant method in order to return an object

--- findARestaurant.py
+++ findARestaurant.py
@@ -65,11 +65,12 @@ def findARestaurant(mealType, location):
                 restaurant_address = restaurant['location']['formattedAddress'][0]
             else:
                 restaurant_address = 'Restaurant Address unavailable.'
-            return {
-                    'name': restaurant_name,
-                    'address': restaurant_address,
-                    'image': picture
-                   }
+            restaurantInfo = {
+                                'name': restaurant_name,
+                                'address': restaurant_address,
+                                'image': picture
+                             }
+            return restaurantInfo
         else:
             return 'Somenthing when wrong. Unable to find a restaurant.\n'
     except Exception as e:
@@ -77,11 +78,3 @@ def findARestaurant(mealType, location):
 
 if __name__ == '__main__':
 	findARestaurant("Pizza", "Tokyo, Japan")
-	findARestaurant("Tacos", "Jakarta, Indonesia")
-	findARestaurant("Tapas", "Maputo, Mozambique")
-	findARestaurant("Falafel", "Cairo, Egypt")
-	findARestaurant("Spaghetti", "New Delhi, India")
-	findARestaurant("Cappuccino", "Geneva, Switzerland")
-	findARestaurant("Sushi", "Los Angeles, California")
-	findARestaurant("Steak", "La Paz, Bolivia")
-	findARestaurant("Gyros", "Sydney, Australia")
