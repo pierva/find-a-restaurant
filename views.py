83453a0ce80e6046b2200414cf1f865c831425d5
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-25 13:35:29 -0500

    feat: Add GET/POST methods for home and /restaurants routes

--- views.py
+++ views.py
@@ -20,11 +20,32 @@ app = Flask(__name__)
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/restaurants', methods = ['GET', 'POST'])
 def all_restaurants_handler():
-    return 'All restaurants'
+    if request.method == 'GET':
+        return getAllRestaurants()
+    elif request.method == 'POST':
+        return createNewRestaurant(name, address, image)
 
 @app.route('/restaurants/<int:id>', methods = ['GET','PUT', 'DELETE'])
 def restaurant_handler(id):
     return 'Restaurant # {}'.format(id)
+
+
+def getAllRestaurants():
+    try:
+        restaurants = session.query(Restaurant).all()
+        return jsonify(Restaurants=[i.serialize for i in restaurants])
+    except Exception as e:
+        return jsonify(Error= {'code': 500})
+
+def createNewRestaurant(name, address, image):
+    try:
+        restaurant = Restaurant(name = name, address = address, image = image)
+        session.add(restaurant)
+        session.commit()
+        return jsonify(Restaurant=restaurant.serialize)
+    except Exception as e:
+        return jsonify(Error= {'code': 500}) 
+
 
 if __name__ == '__main__':
     app.debug = True
