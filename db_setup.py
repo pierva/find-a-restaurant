defd02eeb1062a4c200cc4d576c5ddfef75acfbf
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-25 13:06:47 -0500

    chore: Add db setup

---
+++ db_setup.py
@@ -0,0 +1,27 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(150), nullable=False)
+    address = Column(String(250))
+    image = Column(String(250))
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'address': self.address,
+            'image': self.image
+        }
+
+engine = create_engine('sqlite://restaurants.db')
+
+Base.metadata.create_all(engine)
